0398d9d8abe8234bf85a27d707c1ccf2000ca950
imreallynameless <lw2002@hotmail.ca>
2023-01-21 23:32:05 -0500

    Update sketch.js

--- client/sketch.js
+++ client/sketch.js
@@ -4,12 +4,16 @@ let TSXData;
 let aequitasData;
 let alphaData;
 var dots = [];
+let startTime;
+let currentTime;
+let duration = 240; // duration in seconds
 
 function preload() {
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+<<<<<<< Updated upstream
   const ws = new WebSocket('ws://localhost:443');
 
   ws.addEventListener("open", () => ws.send("start"));
@@ -18,6 +22,9 @@ function setup() {
     console.log(data);
     // handle changes in data
   });
+=======
+  startTime = millis();
+>>>>>>> Stashed changes
   
 }
 
@@ -26,8 +33,43 @@ function windowResized() {
 }
 
 function draw() {
+<<<<<<< Updated upstream
+=======
+  background(255);
+
+  // add new data every second
+  if (frameCount % 360 == 0) {
+    let curData = fakeStream(TSXData, 0, 0);
+
+    for (let i = 0; i < curData.length; i++) {
+      dots.push(dotMaker(curData[i]));
+    }
+    // console.log(dots.length);
+
+    // check overlap of dots
+    // for (let i = 0; i < dots.length; i++) {
+    //   for (let j = 0; j < dots.length; j++) {
+    //     if (i != j) {
+    //       let safety = 0;
+    //       while (dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y) < 10 && safety < 100) {
+    //         dots[i].x = random(width);
+    //         dots[i].y = random(height);
+    //         safety++;
+    //       }
+    //     }
+    //   }
+    // }
+  }
+>>>>>>> Stashed changes
   
   background(31, 31, 31);
+  currentTime = millis();
+  let elapsedTime = currentTime - startTime;
+  let percentageComplete = (elapsedTime / (duration * 1000)) * width;
+  strokeWeight(1);
+  fill(0,255,0);
+  rect(0, 0, percentageComplete, 20);
+
   // update the drawn data
   for (let i = 0; i < dots.length; i++) {
     let transparency = (dots[i].timer/40)*255;
@@ -50,7 +92,7 @@ function draw() {
 function dotMaker(data) {
   var dot = {
     x: random(width),
-    y: random(height),
+    y: random(0,height-20),
     // timer: data["OrderPrice"] / 20,
     timer: Math.random() * 1000 * 6,
     // strokeWeight: (data["OrderPrice"]) / 15
