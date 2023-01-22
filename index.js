7461503d936f51b2ee61c3bafc24a6c7f0e17bf5
meekranjer <logicism81@gmail.com>
2023-01-21 22:56:28 -0500

    volume

--- index.js
+++ index.js
@@ -11,18 +11,29 @@ async function begin(ws) {
     const currentTime = Date.now();
     const elapsedTime = currentTime - startTime;
 
+    //create dict for this second
+    let dict = {};
     while (index < data.length && elapsedTime >= data[index].RelativeMillis) {
       let message = data[index];
       if (message.MessageType === "NewOrderAcknowledged")
         ws.send(JSON.stringify(message));
       index++;
+
+      if (dict[message.Symbol] === undefined) {
+        dict[message.Symbol] = 1;
+      } else dict[message.Symbol] += 1;
     }
+
+    //send dict
+    ws.send(JSON.stringify(dict));
+    dict = {};
 
     await new Promise((r) => setTimeout(r, 1000));
   }
 }
 
 const wss = new WebSocket.Server({ port: 443 });
+// NOTE: This is not yet a secure websocket server (despite the 443 port) - we need to add SSL/TLS to this server (to be able to use wss:// instead of just ws://)
 
 wss.on("connection", (ws) => {
   console.log("A new client has connected");
